5c525a4ff6ec77f1f97a6acccb0c94a799a41909
obviousjim <james@jamesgeorge.org>
2012-10-01 23:24:41 +0900

    added header comments

--- src/ofxLocalization.cpp
+++ src/ofxLocalization.cpp
@@ -1,10 +1,34 @@
-//
-//  ofxLocalization.h
-//  Duration
-//
-//  Created by obvioujim ( http://www.jamesgeorge.org ) @ ycam interlab ( http://interlab.ycam.jp )
-//
-//
+/**
+ * ofxLocalization
+ * simple openFrameworks addon for creating a translation database
+ *
+ * Copyright (c) 2012 James George
+ * Development Supported by YCAM InterLab http://interlab.ycam.jp/en/
+ * http://jamesgeorge.org + http://flightphase.com
+ * http://github.com/obviousjim + http://github.com/flightphase
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 
 #include "ofxLocalization.h"
 
@@ -83,7 +107,7 @@ string ofxLocalization::getCurrentLanguage(){
 
 void ofxLocalization::setCurrentLanguage(string language){
 	if(language == "" || translation.find(language) == translation.end()){
-		ofLogError("ofxLocalization::setCurrentLanguage") << "Couldn't find language" << language;
+		ofLogError("ofxLocalization::setCurrentLanguage") << "Couldn't find language " << language;
 		return;
 	}
 	currentLanguage = language;
@@ -95,7 +119,7 @@ string ofxLocalization::translateKey(string key){
 
 string ofxLocalization::translateKeyToLanguage(string key, string language){
 	if(translation[currentLanguage].find(key) == translation[currentLanguage].end()){
-		ofLogError("ofxLocalization::translateKey") << "Couldn't find key" << key << " in language " << language << endl;
+		ofLogError("ofxLocalization::translateKey") << "Couldn't find key " << key << " in language " << language << endl;
 		return key;
 	}
 	return translation[currentLanguage][key];
